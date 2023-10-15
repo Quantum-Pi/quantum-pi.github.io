8d74ef6f97a4fa3a7ec13c4858415ef07e7aa95f
quantum-pi <quantumpie8@gmail.com>
2023-10-15 16:50:07 -0500

    added Friends component

---
+++ src/components/section/Friends.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { profile } from '$lib/profile';
+</script>
+
+<div class="w-3/4 flex flex-col items-center">
+	<div class="text-white text-4xl mb-4">My Friends</div>
+	<div class="grid grid-cols-5 md:grid-cols-7 lg:grid-cols-12 gap-4">
+		{#each profile.friends.sort((a, b) => a.friend_since - b.friend_since) as friend}
+			<div>
+				<img
+					src={`https://avatars.steamstatic.com/${friend.avatar}`}
+					alt={`${friend.username}'s pfp`}
+				/>
+			</div>
+		{/each}
+	</div>
+</div>
