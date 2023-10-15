7d542851a3c98be1632af1eb842091e7014b6f30
quantum-pi <quantumpie8@gmail.com>
2023-10-15 16:49:32 -0500

    added RecentGame component

---
+++ src/components/section/RecentGames.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { profile } from '$lib/profile';
+	import Game from '../Game.svelte';
+
+	const recentGames = profile.games
+		.filter((game) => game.playtime_2weeks && game.playtime_2weeks > 10)
+		.sort((a, b) => (b.playtime_2weeks ?? 0) - (a.playtime_2weeks ?? 0));
+</script>
+
+<div class="flex flex-col items-center p-8 text-white">
+	<div class="text-4xl mb-8">Currently Playing</div>
+	<div class="grid grid-cols-2 lg:grid-cols-4 gap-8">
+		{#each recentGames as game}
+			{#if game.playtime_2weeks}
+				<Game {game} />
+			{/if}
+		{/each}
+	</div>
+</div>
