13eba082ce1fb95bb03cf4656bb74d83523ebf0d
quantum-pi <quantumpie8@gmail.com>
2023-10-15 16:49:05 -0500

    added Game component

---
+++ src/components/Game.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import type { profile } from '$lib/profile';
+	export let game: (typeof profile.games)[0];
+</script>
+
+<a
+	href={`https://store.steampowered.com/app/${game.appid}/`}
+	class="card card-hover flex flex-col text-white bg-gray-800"
+>
+	<header>
+		<img
+			src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${game.appid}/header.jpg`}
+			alt={`${game.name} header image`}
+		/>
+	</header>
+	<div class="p-4 space-y-4 h-full flex flex-col justify-between">
+		<div class="h3 lg:h4">{game.name}</div>
+		<div class="flex justify-between">
+			<div>
+				<div class="h4">Playtime</div>
+				<div class="h5">
+					{#if game.playtime_2weeks}
+						<div class="">2 weeks: {(game.playtime_2weeks / 60).toFixed(1)}h</div>
+					{/if}
+					<div class="">total: {(game.playtime / 60).toFixed(1)}h</div>
+				</div>
+			</div>
+			<div>
+				{#if game.num_achievements}
+					<div class="h4">Achievements</div>
+					<div class="h5">{game.achievements?.length} / {game.num_achievements}</div>
+				{/if}
+			</div>
+		</div>
+	</div>
+	<hr class="opacity-50" />
+	<footer class="flex items-center space-x-4 p-2">
+		<img
+			src={`http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.icon_url}.jpg`}
+			alt={`${game.name} icon`}
+		/>
+		<div>Last played {new Date(game.last_played * 1000).toLocaleDateString('en-US')}</div>
+	</footer>
+</a>
