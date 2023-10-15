662f558a9a17cfe485392fc2147cf6e806efcad1
quantum-pi <quantumpie8@gmail.com>
2023-10-15 16:50:49 -0500

    set homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Footer from '../components/section/Footer.svelte';
+	import Friends from '../components/section/Friends.svelte';
+	import Hero from '../components/section/Hero.svelte';
+	import MostPlayed from '../components/section/MostPlayed.svelte';
+	import RecentGames from '../components/section/RecentGames.svelte';
+</script>
+
+<!-- <div id="content" class="h-[1500px]"> -->
+<Hero />
+<div id="content" class="px-8 flex flex-col items-center">
+	<RecentGames />
+	<MostPlayed />
+	<Friends />
+	<Footer />
+</div>
+
+<!-- </div> -->
+
+<style>
+	#content {
+		background: linear-gradient(#0b0032, #000000);
+	}
+</style>
