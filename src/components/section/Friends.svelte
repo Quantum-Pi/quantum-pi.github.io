8784349c1a3b66ce34ac4fcb4672df130ef5293f
quantum-pi <quantumpie8@gmail.com>
2023-10-16 14:36:08 -0500

    added interactivity to friends section

--- src/components/section/Friends.svelte
+++ src/components/section/Friends.svelte
@@ -6,12 +6,28 @@
 	<div class="text-white text-4xl mb-4">My Friends</div>
 	<div class="grid grid-cols-5 md:grid-cols-7 lg:grid-cols-12 gap-4">
 		{#each profile.friends.sort((a, b) => a.friend_since - b.friend_since) as friend}
-			<div>
+			<a
+				href={`https://steamcommunity.com/profiles/${friend.steamid}`}
+				title={friend.username}
+				class="profile-picture"
+			>
 				<img
 					src={`https://avatars.steamstatic.com/${friend.avatar}`}
 					alt={`${friend.username}'s pfp`}
 				/>
-			</div>
+			</a>
 		{/each}
 	</div>
 </div>
+
+<style>
+	.profile-picture {
+		transition: 0.25s;
+	}
+	.profile-picture:hover {
+		transform: scale(1.05);
+	}
+	.profile-picture:active {
+		transform: scale(0.95);
+	}
+</style>
