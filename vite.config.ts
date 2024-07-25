2631b5355c06e6419650e54fc315be822eacc6a7
quantum-pi <quantumpie8@gmail.com>
2024-07-25 11:28:29 -0500

    updated sveltekit, added enhanced-image

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		enhancedImages(),
 		Icons({
 			compiler: 'svelte'
 		})
