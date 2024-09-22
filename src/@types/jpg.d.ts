6e02803a79b2c0d802a1f1122ddca57e8e060d1c
quantum-pi <quantumpie8@gmail.com>
2024-09-22 18:33:58 -0500

    version bump, fix jpg import error

---
+++ src/@types/jpg.d.ts
@@ -0,0 +1,13 @@
+declare module '*&format=webp' {
+	import type { Picture } from 'vite-imagetools';
+	const value: Picture;
+
+	export default value;
+}
+
+declare module '*&w=64;128;200' {
+	import type { Picture } from 'vite-imagetools';
+	const value: Picture;
+
+	export default value;
+}
