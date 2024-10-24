8944cf2765f54dfb7df5a562991480a8ba640e68
quantum-pi <quantumpie8@gmail.com>
2024-10-24 14:44:22 -0500

    added casual game selector section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
 	import Achievements from '../components/section/Achievements.svelte';
-	import Eso from '../components/games/ESO/Overview_ESO.svelte';
 	import Footer from '../components/section/Footer.svelte';
 	import Friends from '../components/section/Friends.svelte';
 	import Hero from '../components/section/Hero.svelte';
 	import MostPlayed from '../components/section/MostPlayed.svelte';
 	import RecentGames from '../components/section/RecentGames.svelte';
 	import GameSelector from '../components/games/GameSelector.svelte';
-	import CharacterCard from '../components/genshin/CharacterCard/CharacterCard.svelte';
-	import genshinData from '$lib/genshin_agg';
+	import GameSelectorCasual from '../components/games/GameSelectorCasual.svelte';
 </script>
 
 <Hero />
 <div id="content">
 	<div class="px-8 flex flex-col items-center">
-		<!-- <CharacterCard character={genshinData[0]}/> -->
 		<div class="mb-24">
 			<RecentGames />
 		</div>
@@ -29,6 +26,9 @@
 <div class="bg-black">
 	<div class="bg-black mb-24 w-full">
 		<GameSelector />
+	</div>
+	<div class="bg-black mb-24 w-full">
+		<GameSelectorCasual />
 	</div>
 	<div class="bg-black px-8 flex flex-col items-center">
 		<Friends />
