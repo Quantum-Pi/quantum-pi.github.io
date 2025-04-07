17df5b7e5cc5be868734a83d7f3ea657a6a8f3e3
Quantum-Pi <quantumpie8@gmail.com>
2025-04-07 12:10:34 -0400

    removed redundent weapons

--- src/components/games/Genshin/Overview_Genshin.svelte
+++ src/components/games/Genshin/Overview_Genshin.svelte
@@ -8,6 +8,7 @@
 	import type { GenshinCharacter, GenshinWeapon } from '$lib/genshin_agg';
 	import MiniWeapon from './MiniWeapon.svelte';
 	import MiniArtifact from './MiniArtifact.svelte';
+	import type { WeaponCacheKey } from '$lib/genshin_cache';
 
 	const sortMiniCharacter = (a: GenshinCharacter, b: GenshinCharacter) => {
 		// 1) Rarity
@@ -35,10 +36,40 @@
 		return b.stars - a.stars;
 	};
 
+	const weaponMaxCache = new Map<WeaponCacheKey, {level: number, refinement: number, added: boolean}>();
+
 	const filterMiniWeapon = (w: GenshinWeapon) => {
-		if (w.stars === 3 && w.level > 1) return true;
-		if (w.stars >= 4) return true;
-		return false;
+		let valid = false
+		if (w.stars === 3 && w.level > 1) valid = true;
+		if (w.stars >= 4) valid = true;
+		
+		if (valid) {
+			if (weaponMaxCache.has(w.key)) {
+				const cache = weaponMaxCache.get(w.key);
+				if (cache && (cache.level < w.level || (cache.level === w.level && cache.refinement < w.refinement))) {
+					weaponMaxCache.set(w.key, { level: w.level, refinement: w.refinement, added: false });
+				}
+			} else {
+				weaponMaxCache.set(w.key, { level: w.level, refinement: w.refinement, added: false });
+			}
+		}
+		return valid;
+	};
+
+	const filterDuplicateWeapons = (w: GenshinWeapon) => {
+		const cache = weaponMaxCache.get(w.key);
+		if (cache) {
+			if (w.location) {
+				return true;
+			}
+			if (cache.level === w.level && cache.refinement === w.refinement && !cache.added) {
+				cache.added = true;
+				weaponMaxCache.set(w.key, cache);
+				return true;
+			}
+			return false
+		}
+		
 	};
 </script>
 
@@ -68,7 +99,7 @@
 		<div
 			class="w-fit h-full grid gap-6 grid-cols-4 sm:grid-cols-6 md:grid-cols-7 lg:grid-cols-9 xl:grid-cols-10 mb-8"
 		>
-			{#each weapons.filter(filterMiniWeapon).sort(sortMiniWeapon) as w}
+			{#each weapons.filter(filterMiniWeapon).sort(sortMiniWeapon).filter(filterDuplicateWeapons) as w}
 				<MiniWeapon weapon={w} />
 			{/each}
 		</div>
