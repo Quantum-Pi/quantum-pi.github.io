476cb5e5d5ec4a8d1d7d482d3b054fe834239ae7
Quantum-Pi <quantumpie8@gmail.com>
2025-04-07 12:18:52 -0400

    improved genshin card stat display

--- src/components/games/Genshin/CharacterCard/Stats/Stat.svelte
+++ src/components/games/Genshin/CharacterCard/Stats/Stat.svelte
@@ -9,16 +9,24 @@
 
 	const valueNum = value ? parseFloat(value.replace(/[^0-9]/g, '')) : undefined;
 	const isEleDmgBonus = icon.includes('DamageBonus') && !icon.includes('physical');
+
+	const statNameCondencer = (name: string, extra: boolean = false) => {
+		let newName = name.replace('DMG Bonus', 'Bonus');
+		return extra
+			? newName.replace('Energy Recharge', 'ER').replace('Elemental Mastery', 'EM')
+			: newName;
+	};
 </script>
 
 {#if valueNum && valueNum > 0}
 	<span class="mb-2 flex items-center"
 		><i
-			class={`${icon} align-middle text-md mr-2 mt-1`}
+			class={`${icon} align-middle text-md mr-2 mt-[2px]`}
 			style={`color: ${isEleDmgBonus ? 'var(--element)' : 'white'}`}
 		>
 			<span class="path1"></span><span class="path2"></span>
-		</i><span>{name.replace('DMG Bonus', 'Bonus')}</span></span
-	>
+		</i>
+		<span>{statNameCondencer(name, true)}</span>
+	</span>
 	<span class="ml-3">{value}</span>
 {/if}
