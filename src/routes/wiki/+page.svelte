8fe900bf34b2c2b5272de95d533e11034a35a998
Quantum-Pi <quantumpie8@gmail.com>
2025-04-13 11:57:52 -0400

    wiki styling

--- src/routes/wiki/+page.svelte
+++ src/routes/wiki/+page.svelte
@@ -2,8 +2,20 @@
 	import Markdown from "../../components/wiki/Markdown.svelte";
 </script>
 
-<div class="flex justify-center">
-    <div class="w-5/6">
-        <Markdown urlSource={true} markdown='https://raw.githubusercontent.com/Quantum-Pi/genshin-summary/refs/heads/main/guide.md'/>    
+<div class="container flex justify-center relative min-w-screen h-screen">
+    <div class="content flex justify-center w-[80vw] h-screen overflow-y-scroll bg-gray-900">
+        <div class="w-5/6 pt-16">
+            <Markdown urlSource={true} markdown='https://raw.githubusercontent.com/Quantum-Pi/genshin-summary/refs/heads/main/guide.md'/>    
+        </div>
     </div>
+    <span class="bg fixed w-screen h-screen top-0 left-0 -z-10"></span>
 </div>
+
+<style>
+    .container {
+        & .bg {
+            background-image: url(/wiki-bg.jpg);
+            position: fixed;
+        }
+    }
+</style>
